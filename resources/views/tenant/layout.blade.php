357a000376c4bf1aff3ece434e07a0a701ee8623
Leonardo DEV <contato@innsystem.com.br>
2026-03-06 09:07:30 -0300

    Test SDK

--- resources/views/tenant/layout.blade.php
+++ resources/views/tenant/layout.blade.php
@@ -24,6 +24,29 @@
         .badge-platform-facebook  { background: #1877f2; }
     </style>
 
+    
+    <script>
+    window.fbAsyncInit = function() {
+        FB.init({
+        appId      : '1614984229637742',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v25.0'
+        });
+        
+        FB.AppEvents.logPageView();   
+        
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
+
     @stack('head')
 </head>
 <body>
